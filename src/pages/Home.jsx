05a51e53b85fc012a5ce6eba925821d25f932751
Patrick Dacius <patrickdacius27@gmail.com>
2025-07-25 12:37:45 -0400

    minimal home page work, i honestly need the photos to flesh out the home page as much as i would like. moving onto about page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
-export default function Home(){
+const  Home = ()=> {
     return (
-        <div>
-            <h2>Welcome to VisionDeSaint Photography</h2>
-            <p>RN by day, Photographer by passion.</p>
+        <div className="home">
+            <section className="hero">
+                <div className="hero-content">
+                    <h2>Welcome to VisionDeSaint Photography</h2>
+                    <h1>Shoot with Vision. Create with Saint.</h1>
+                    <p>NYC based Photographer specializing in lifestyle, portraits, events, and creative shoots.</p>
+                    <p>RN by day, Photographer by passion.</p>
+                    
+                    <div className="hero-buttons">
+                        <button>View Gallery</button>
+                        <button>Book a Shoot</button>
+                    </div>
+                </div>
+            </section>
+
+           {/*About section*/}
+
+           
 
         </div>
     );
 }
+
+
+export default Home
